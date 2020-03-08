dd66edbf674d12518d5ae26f69cc2554ceccabe9
hcyhehe <960696163@qq.com>
2020-03-08 23:02:29 +0800

    3333

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,54 @@ import Helmet from 'react-helmet'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import routes from './routes'
 
+const App = props => {
 
-function App() {
+  // 解构 route
+  function renderRoutes(routes, contextPath) {
+    const children = []
+
+    const renderRoute = (item, routeContextPath) => {
+      let newContextPath = item.path ? `${routeContextPath}/${item.path}` : routeContextPath
+      newContextPath = newContextPath.replace(/\/+/g, '/')
+      if (newContextPath.includes('admin')) {
+        item = {
+          ...item,
+          component: () => <Redirect to='/' />,
+          children: []
+        }
+      }
+
+      if (item.component) {
+        if (item.childRoutes) {
+          const childRoutes = renderRoutes(item.childRoutes, newContextPath)
+          children.push(
+            <Route
+              key={newContextPath}
+              render={props => <item.component {...props}>{childRoutes}</item.component>}
+              path={newContextPath}
+            />
+          )
+          item.childRoutes.forEach(r => renderRoute(r, newContextPath))
+        } else {
+          children.push(<Route key={newContextPath} component={item.component} path={newContextPath} exact />)
+        }
+      }
+    }
+
+    routes.forEach(item => renderRoute(item, contextPath))
+
+    return <Switch>{children}</Switch>
+  }
+
+  const children = renderRoutes(routes, '/')
   return (
     <BrowserRouter>
       <Helmet>
         <title>react project</title>
       </Helmet>
-      {routes}
+      {children}
     </BrowserRouter>
   )
-  
 }
 
 export default App
